0d4207f3e867abef716478be3e0c24614143e296
Vipin Gopinath <vipin.vipin.g498@gmail.com>
2017-07-07 12:46:05 +0530

    Updated congif

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
-    database:"mongodb://viping7:highveld@ds147902.mlab.com:47902/meanauth",
-    secret:"highveld"
+    database:"mongodb://<dbusername>:<dbpassword>@ds147902.mlab.com:47902/meanauth",
+    secret:"******"
     
 }
